984162214ae1ceecb938b4cce728f224cd2f0fcd
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-12-06 17:41:45 +0900

    first commit

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-full bg-slate-400 ">
+      <div className="flex justify-around ">
+        <p>Biology books</p>
+        <p>Scince books</p>
+        <p>Literature books</p>
+        <p>Language books</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
